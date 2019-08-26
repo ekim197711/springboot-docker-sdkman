1a67f0e008010496b91d5a24b00bbfaff9127c41
Mike Møller Nielsen <mmni@protonmail.com>
2019-08-26 21:49:46 +0200

    The files

---
+++ src/main/java/com/codeinvestigator/demodockersdkman/DemoDockerSdkmanApplication.java
@@ -0,0 +1,13 @@
+package com.codeinvestigator.demodockersdkman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDockerSdkmanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoDockerSdkmanApplication.class, args);
+    }
+
+}
